2b2101a84ddbe52649a248d55a89e7d4f1a3af52
Max Brown <max.brown@ed.ac.uk>
2020-06-14 22:13:44 +0100

    Delete unnecessary d3, and update minify

--- dist/make_min.sh
+++ dist/make_min.sh
@@ -2,8 +2,4 @@
 
 # Command that I used to install minify: npm i minify -g
 
-minify ../src/*.js > ./lwPhylo.min.js
-
-# remove trailing white-spaces
-
-cat lwPhylo.min.js | tr -d "\n" > lwPhylo.min.js
+minify ../src/*.js | tr -d "\n" > ./lwPhylo.min.js
